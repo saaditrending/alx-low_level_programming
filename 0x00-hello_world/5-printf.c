f2bf3b7596f8e4b57597a03b066f7757f26b449b
saaditrending@gmail.com <saaditrending>
2023-06-18 05:17:39 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Program that prints aline with prntf
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
