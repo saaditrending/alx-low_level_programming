0d294324ada7c75d37654825007c9ed17025fa3c
saaditrending@gmail.com <saaditrending>
2023-06-18 09:03:36 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ * MAIN - a program that prints how art is usefull
+ * Return: (1)
+ */
+int main (1)
+{
+	putchar(and that piece of art is useful" - Dora Korpar, 2015-10-19);
+	return (1) ;	
