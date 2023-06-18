2258bbdd5e50989b9abf79882d6660c7f0487702
saaditrending@gmail.com <saaditrending>
2023-06-18 04:51:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Aprogram that prints a line with puts
+ * Return: void
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
