e45c582221f3b8797c70ad8bd76a51f9ad244858
saaditrending@gmail.com <saaditrending>
2023-06-18 13:19:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
  * MAIN - a program that prints how art is usefull
  * Return: (1)
  */
-int main (1)
+
+int main(void)
 {
-	putchar(and that piece of art is useful" - Dora Korpar, 2015-10-19);
-	return (1) ;	
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return(1);
+}
