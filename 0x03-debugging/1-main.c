829264dba742ea6c75412ae150119ea5f14fb51f
saaditrending@gmail.com <saaditrending>
2023-06-22 08:55:38 -0700

    this is debugging

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - program for this infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int z;
+
+	printf("descending Infinite loop :(\n");
+
+	/*z = 0;*/
+
+	/*while (z < 10)*/
+	/*{*/
+		/*putchar(z);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
