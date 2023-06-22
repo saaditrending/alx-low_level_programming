f25e55e05830ef9aab78883b96f3054034e62c15
saaditrending@gmail.com <saaditrending>
2023-06-22 01:24:32 -0700

    This program will assign a random number to the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,30 +1,33 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
 
 /**
- * main - a program will assign a random number to the variabl
- * Return : void
+ * main - program that prints  positive, negative and zero
+  *Return: Alway (0)
  */
 
 int main(void)
 
 {
+
 	int n;
 
-	if(n > 0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
 	{
-	printf("%d is postive\n", n);
+		printf("%d is positive\n", n);
 	}
-        elseif (n < 0);
+	else if (n < 0)
 	{
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-        printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-        return (0);
+	return (0);
 }
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,28 +3,37 @@
 #include <stdio.h>
 
 /**
- * main - a program that will assign numbers to a variable with last digit n.
- *Reeturn:(void)
+ * main - This program will assign a random number to the variable n
+ * Return: Always 0 (Sucess)
  */
-int main(void)
 
-	{
+int main(void)
+{
 	int n;
 
-if (n > 5)
-	{
-printf( %d and is greater than 5\n", n);
-	}
-elseif (n < 6 );
-	{
-printf( %d and is less than 6 and not 0\n", n);
-	}
-else(n = 0);
-	{
-printf( %d and is 0\n", n);
-	}
-srand(time(0));
+	int n;
+{
+	int n, h;
+
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* if(n > 0 ; print n */
+	/* yor code goes there */
+	h = n % 10;
+
+	if (h > 5)
+	{
+	printf("Last digit of %d is %d and is greater than 5\n", n, h);
+	if (h == 0)
+	}
+	else if (h == 0)
+	{
+	printf("Last digit of %d is %d and is 0\n", n, h);
+	if (h < 6 && h != 0)
+	}
+	else if (h < 6 && h != 0)
+	{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, h);
+	}
 	return (0);
 }
+
