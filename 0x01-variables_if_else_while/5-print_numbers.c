d30ce6111fe620a31e7ca9ff013cb2ca3eb3bec5
saaditrending@gmail.com <saaditrending>
2023-06-21 23:54:39 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single numbers of base 10 from 0
+ * Return: (0)
+ */
+int main(void)
+{
+	int hf;
+
+	for (hf = 0; hf <= 9; hf++)
+		printf("%d", hf);
+	printf("\n");
+
+	return (0);
+}
