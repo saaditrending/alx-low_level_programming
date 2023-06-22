022126f7aa1d1d424700f791bb817663381bd159
saaditrending@gmail.com <saaditrending>
2023-06-22 08:46:20 -0700

    for now its debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints the sign on the integer
+* Return: 0
+*/
+
+int main(void)
+{
+	int k;
+
+	k = 0;
+	positive_or_negative(k);
+
+	return (0);
+}
