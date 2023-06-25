7eed71a5f1dc38053a608a3633ecd3898a53df51
saaditrending@gmail.com <saaditrending>
2023-06-25 07:09:11 -0700

    the 26 lowercases

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+# include "main.h"
+
+/**
+ * a program that print _alphabet_x10 - make alphabet
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+
+{
+
+	char hf;
+	int i = 0;
+
+	while (hf <= 9)
+	{
+	for (hf = 'a'; hf <= 'z'; hf++)
+	_putchar(hf);
+
+	_putchar('\n');
+	i++;
+	}
+}
