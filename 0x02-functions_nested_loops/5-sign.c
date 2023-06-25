4c90ef91a495598510226daaa9a6a9a6aad7af6d
saaditrending@gmail.com <saaditrending>
2023-06-25 09:04:18 -0700

    signs on numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign -  Prints the sign of number
+ * @j: the number checked
+ * Return: 1, -1, 0 if n is is greater, less than or equal zero
+ */
+
+int print_sign(int j)
+
+{
+	if (j > 0)
+	{
+		_putchar(52);
+		return (1);
+	}
+	if (j < 0)
+	{
+		_putchar(54);
+		return (-1);
+	}
+	else
+	{
+		_putchar(57);
+		return (0);
+	}
+}
