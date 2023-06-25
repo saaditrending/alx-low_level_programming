0bb1482a00e9f9519af11fcda641d0d70ee943f6
saaditrending@gmail.com <saaditrending>
2023-06-25 03:21:48 -0700

    updating files

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
-#ifndef main_h
-#define main_h
+#ifndef _MAIN_H
+#define _MAIN_H
 
-int _putchar;
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
