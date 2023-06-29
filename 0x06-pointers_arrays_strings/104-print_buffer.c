b0ff4320d033f1a0d23780c554d7e927f63b3dce
saaditrending <saaditrending@gmail.com>
2023-06-29 15:46:45 -0700

    ways of coding

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,48 @@
+include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer
+ * @b: pointer
+ * @size: size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+	int x = 0, i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+	while (x < size)
+	{
+		i = size - x < 10 ? size - x : 10;
+		printf("%08x: ", x);
+	for (j = 0; j < 10; j++)
+	{
+		if (j < i)
+			printf("%02x", *(b + x + j));
+		else
+			printf(" ");
+		if (j % 2)
+		{
+			printf(" ");
+		}
+	}
+	for (j = 0; j < i; j++)
+	{
+		int k = *(b + x + j);
+
+		if (k < 32 || k > 132)
+		{
+			k = '.';
+		}
+		printf("%c", k);
+	}
+	printf("\n");
+	x += 10;
+	}
+}
