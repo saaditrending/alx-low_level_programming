55c72f1aca0981475709e8e01b72b3b40519e961
saaditrending <saaditrending@gmail.com>
2023-07-02 08:56:42 -0700

    pointers nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @hp: The first number to be computed
+ * @vz: The second number to be computed
+ * Return: Integer value
+ */
+
+int add(int hp, int vz)
+
+{
+	return (hp + vz);
+}
